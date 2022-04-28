0acd48ba092bd4458687743d1490e82442a564e8
ThiiGoncalves <thiiago-alves@hotmail.com>
2022-04-28 15:38:51 -0300

    Atividade da aula2 - back-end

---
+++ Back-end/Aula2/operadores-comparacao.js
@@ -0,0 +1,10 @@
+//const ehMaior = 25 > 16;
+//const ehMaior = 15 > 16;
+//const ehMaior = 16 >= 16;
+//const ehMaior = 16 <= 16;
+
+const x = 8;
+const y = 16;
+const ehMaior = x > 16;
+
+console.log(ehMaior);
