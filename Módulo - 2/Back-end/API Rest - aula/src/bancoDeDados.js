ea3a02c0d930d92995f1c3afcf845a82a1bf3ea7
ThiiGoncalves <thiiago-alves@hotmail.com>
2022-07-20 10:33:28 -0300

    Pratica de API Rest - backend

---
+++ Módulo - 2/Back-end/API Rest - aula/src/bancoDeDados.js
@@ -0,0 +1,20 @@
+const bancoDeDados = {
+    identificadorInstrutor: 3,
+    instrutores: [
+        {
+            id: 1,
+            nome: 'Thiago',
+            email: 'thiago@email.com',
+            status: true
+        },
+        {
+            id: 2,
+            nome: 'Dayane',
+            email: 'dayane@email.com',
+            status: true
+        }
+
+    ]
+};
+
+module.exports = bancoDeDados;
